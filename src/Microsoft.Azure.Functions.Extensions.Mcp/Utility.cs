5ad87a5f124ef126b60e5ebbdc877782d12061f6
Fabio Cavalcante <facaval@microsoft.com>
2025-05-15 14:35:54 -0700

    Restricting ID generation to a set of valid characters (#49)

--- src/Microsoft.Azure.Functions.Extensions.Mcp/Utility.cs
+++ src/Microsoft.Azure.Functions.Extensions.Mcp/Utility.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.WebUtilities;
 
 
@@ -6,10 +7,27 @@ namespace Microsoft.Azure.Functions.Extensions.Mcp;
 
 internal class Utility
 {
+    private const string ValidChars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    private const int MaxByte = 252; // 252 is the largest multiple of 36 (the valid chars array) under 256
+    private const int Length = 16;
+
     internal static string CreateId()
     {
-        Span<byte> buffer = stackalloc byte[16];
-        RandomNumberGenerator.Fill(buffer);
-        return WebEncoders.Base64UrlEncode(buffer);
+        Span<char> result = stackalloc char[Length];
+        Span<byte> buffer = stackalloc byte[1];
+        var count = 0;
+
+        while (count < Length)
+        {
+            RandomNumberGenerator.Fill(buffer);
+            var value = buffer[0];
+
+            if (value < MaxByte)
+            {
+                result[count++] = ValidChars[value % ValidChars.Length];
+            }
+        }
+
+        return new string(result);
     }
 }
