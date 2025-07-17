abcf86ddea0e2aa5451a71013aabc8a003ca6e5a
Matthew Henderson <mattchenderson@users.noreply.github.com>
2025-07-17 14:18:51 -0700

    initial end-to-end

---
+++ test/Extensions.Mcp.EndToEnd/Fixtures/McpEndToEndProjects.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Extensions.Mcp.EndToEnd.AbstractionOverCoreTools;
+
+namespace Microsoft.Azure.Functions.Extensions.Mcp.EndToEnd.Fixtures;
+
+internal class McpEndToEndProjects
+{
+    private static string RepoRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"../../../.."));
+    private static string RelativePathForTestName(string projectName) => Path.Combine(RepoRoot, "out", "bin", projectName, "debug");
+
+    internal class DotnetWorkerProject : EndToEndTestProject
+    {
+        [System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
+        public DotnetWorkerProject(string projectName) : base()
+        {
+            ProjectDirectoryPath = Path.Combine(RepoRoot, RelativePathForTestName(projectName));
+            FunctionsWorkerRuntime = "dotnet-isolated";
+        }
+    }
+
+    internal class InProcNet8Project : EndToEndTestProject
+    {
+        [System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
+        public InProcNet8Project(string projectName) : base()
+        {
+            ProjectDirectoryPath = Path.Combine(RepoRoot, RelativePathForTestName(projectName));
+            FunctionsWorkerRuntime = "dotnet";
+            AdditionalCoreToolsArguments = ["--runtime", "inproc8"];
+            LaunchEnvironmentVariables = new Dictionary<string, string> { { "FUNCTIONS_INPROC_NET8_ENABLED", "1" } }; // This needs to be in the local.settings.json. Cannot rely on env var for now.
+        }
+    }
+}
