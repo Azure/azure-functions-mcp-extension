7bc0aa5befbe26e4ac653d49aa6702cd04c02ae6
Lilian Kasem <likasem@microsoft.com>
2025-07-24 11:45:34 -0700

    Add dotnet-isolated test app (#76)

---
+++ test/TestAppIsolated/ToolsInformation.cs
@@ -0,0 +1,10 @@
+namespace TestAppIsolated;
+
+internal sealed class ToolsInformation
+{
+    public const string GetSnippetToolName = "getsnippets";
+    public const string GetSnippetToolDescription = "Gets code snippets from your snippet collection.";
+    public const string SnippetNamePropertyName = "snippetname";
+    public const string SnippetNamePropertyDescription = "The name of the snippet.";
+    public const string PropertyType = "string";
+}
