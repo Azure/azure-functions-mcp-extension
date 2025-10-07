7762cd4343b70e5971336b51b9eac3399bc8370e
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-10-07 13:40:20 -0700

    refactoring to worker project

--- test/Worker.Mcp.E2ETests/Constants.cs
+++ test/Worker.Mcp.E2ETests/Constants.cs
@@ -1,6 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Microsoft.Azure.Functions.Worker.Mcp.E2ETests
 {
     internal static class Constants
