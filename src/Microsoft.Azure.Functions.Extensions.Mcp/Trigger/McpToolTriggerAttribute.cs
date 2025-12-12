d758802a8027c8a85e5ec53f9c5c9ff140d2f71a
Aishwarya Bhandari <aibhandari@microsoft.com>
2025-12-12 11:42:38 -0800

    removing unneccessary values

--- src/Microsoft.Azure.Functions.Extensions.Mcp/Trigger/McpToolTriggerAttribute.cs
+++ src/Microsoft.Azure.Functions.Extensions.Mcp/Trigger/McpToolTriggerAttribute.cs
@@ -1,11 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.Azure.Functions.Extensions.Mcp.Trigger;
 using Microsoft.Azure.WebJobs.Description;
-using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
 
 namespace Microsoft.Azure.Functions.Extensions.Mcp;
 
