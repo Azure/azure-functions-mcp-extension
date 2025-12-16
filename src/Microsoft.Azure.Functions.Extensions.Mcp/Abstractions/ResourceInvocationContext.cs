4561ab19e653774214dae0316474dd9678a898ce
Lilian Kasem <likasem@microsoft.com>
2025-12-15 16:06:42 -0800

    Implement trigger binding & listener

---
+++ src/Microsoft.Azure.Functions.Extensions.Mcp/Abstractions/ResourceInvocationContext.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json.Serialization;
+using ModelContextProtocol.Protocol;
+
+namespace Microsoft.Azure.Functions.Extensions.Mcp;
+
+internal sealed class ResourceInvocationContext
+{
+    /// <summary>
+    /// Gets or sets the URI of the resource to invoke.
+    /// </summary>
+    [JsonPropertyName("uri")]
+    public required string Uri { get; init; }
+
+    /// <summary>
+    /// Gets the session ID associated with the current resource invocation.
+    /// </summary>
+    [JsonPropertyName("sessionId")]
+    public string? SessionId { get; init; }
+
+    /// <summary>
+    /// Gets the MCP client information.
+    /// </summary>
+    [JsonPropertyName("clientInfo")]
+    public Implementation? ClientInfo { get; init; }
+
+    /// <summary>
+    /// Gets the transport information.
+    /// </summary>
+    [JsonPropertyName("transport")]
+    public Transport? Transport { get; init; }
+}
---
+++ src/Microsoft.Azure.Functions.Extensions.Mcp/Trigger/McpResourceListener.cs
@@ -0,0 +1,69 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.WebJobs.Host.Executors;
+using Microsoft.Azure.WebJobs.Host.Listeners;
+using ModelContextProtocol.Protocol;
+using ModelContextProtocol.Server;
+
+namespace Microsoft.Azure.Functions.Extensions.Mcp;
+
+internal sealed class McpResourceListener(ITriggeredFunctionExecutor executor,
+                                          string functionName,
+                                          string resourceUri,
+                                          string resourceName,
+                                          string? resourceDescription,
+                                          string? resourceMimeType,
+                                          long? resourceSize,
+                                          IReadOnlyCollection<KeyValuePair<string, object?>>? metadata) : IListener, IMcpResource
+{
+    public ITriggeredFunctionExecutor Executor { get; } = executor;
+
+    public string FunctionName { get; } = functionName;
+
+    public string Uri { get; } = resourceUri;
+
+    public string Name { get; set; } = resourceName;
+
+    public string? Description { get; set; } = resourceDescription;
+
+    public string? MimeType { get; set; } = resourceMimeType;
+
+    public long? Size { get; set; } = resourceSize;
+
+    public IReadOnlyCollection<KeyValuePair<string, object?>>? Metadata { get; set; } = metadata;
+
+    public void Dispose() { }
+
+    public Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    public void Cancel() { }
+
+    public async Task<ReadResourceResult> RunAsync(RequestContext<ReadResourceRequestParams> readResourceRequest, CancellationToken cancellationToken)
+    {
+        var execution = new ReadResourceExecutionContext(readResourceRequest);
+
+        var input = new TriggeredFunctionData
+        {
+            TriggerValue = execution
+        };
+
+        var result = await Executor.TryExecuteAsync(input, cancellationToken);
+
+         if (!result.Succeeded)
+        {
+            throw result.Exception;
+        }
+
+        var resourceResult = await execution.ResultTask;
+
+        if (resourceResult is ReadResourceResult readResourceResult)
+        {
+            return readResourceResult;
+        }
+
+        return new ReadResourceResult { Contents = [] };
+    }
+}
