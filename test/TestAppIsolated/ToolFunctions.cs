3610a27b3436b6901d69f3c037aa0e7f5a8579b4
Aishwarya Bhandari <aibhandari@microsoft.com>
2026-01-06 12:47:53 -0800

    fixing syntax error

--- test/TestAppIsolated/ToolFunctions.cs
+++ test/TestAppIsolated/ToolFunctions.cs
@@ -167,7 +167,7 @@ public class TestFunction
 
     public class UserInfo
     {
-        public string UserId { get; set; }
+        public required string UserId { get; set; }
         public string? Name { get; set; }
         public int Age { get; set; }
         public List<string>? Hobbies { get; set; }
