17df1d3e6f11d86e6e3f650648c1aba8d145f9f7
Aishwarya Bhandari <aibhandari@microsoft.com>
2026-01-08 14:49:09 -0800

    updating release notes

--- src/Microsoft.Azure.Functions.Worker.Extensions.Mcp.Sdk/FunctionsMiddleware/FunctionsMcpToolResultMiddleware.cs
+++ src/Microsoft.Azure.Functions.Worker.Extensions.Mcp.Sdk/FunctionsMiddleware/FunctionsMcpToolResultMiddleware.cs
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections;
+using System.Text.Json;
 using Microsoft.Azure.Functions.Worker.Extensions.Mcp.Sdk;
 using Microsoft.Azure.Functions.Worker.Middleware;
 using ModelContextProtocol;
 using ModelContextProtocol.Protocol;
-using System.Collections;
-using System.Text.Json;
 
 namespace Microsoft.Azure.Functions.Worker.Extensions.Mcp;
 
